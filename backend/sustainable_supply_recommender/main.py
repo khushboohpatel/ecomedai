fb46a1c4fccca6736e4a846b2498af522ff0d64e
Rishit Epari <epari.rishit@gmail.com>
2025-03-02 00:35:54 -0500

    Fix the api launch

--- backend/sustainable_supply_recommender/main.py
+++ backend/sustainable_supply_recommender/main.py
@@ -121,4 +121,4 @@ if __name__ == "__main__":
     if args.mode == "cli":
         run_cli()
     else:
-        uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
